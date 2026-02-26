6a5815d6356adbbe82bbf4e694c46c9b0da8e32b
DUSHINTHAN <vankatesh.dushinthan@gmail.com>
2026-02-26 19:49:01 +0530

    Added projection and added hql and sql query methods and tested

--- src/main/java/com/scaler/productservice/repositories/ProductRepository.java
+++ src/main/java/com/scaler/productservice/repositories/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.scaler.productservice.repositories;
 
 import com.scaler.productservice.models.Product;
+import com.scaler.productservice.repositories.projections.ProductTitleAndDescription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +12,24 @@ import java.util.Optional;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Product save(Product product);
+
     Optional<Product> findById(long id);
+
     List<Product> findAll();
+
+    Optional<Product> findByTitleAndCategory_Name(String title, String categoryName);
+
+    @Query("SELECT p FROM Product p WHERE p.category.name = :categoryName")
+    List<Product> getByCategoryName(@Param("categoryName") String categoryName);
+
+    @Query(value = "SELECT * FROM product p JOIN category c ON p.category_id = c.id WHERE c.name = :categoryName", nativeQuery = true)
+    List<Product> getByCategoryNameNative(@Param("categoryName") String categoryName); //(same as above but using native query)
+
+    @Query(value = "SELECT * FROM product WHERE id = :id", nativeQuery = true)
+    Product getProductDetail2(@Param("id") long id); // (same as findById but using native query)
+                                                     //we could have return type as Optional<Product> but for simplicity we have kept it product only as return type and we are not handling the case
+                                                     // when the product is not found in the database but in real world application we should handle that case as well
+
+    @Query(value = "SELECT title, description FROM product WHERE id = :id", nativeQuery = true)
+    ProductTitleAndDescription getProductDetail3(@Param("id") long id);
 }
--- src/test/java/com/scaler/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/scaler/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,57 @@
 package com.scaler.productservice;
 
+import com.scaler.productservice.models.Product;
+import com.scaler.productservice.repositories.ProductRepository;
+import com.scaler.productservice.repositories.projections.ProductTitleAndDescription;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
+
+	@Autowired
+	ProductRepository productRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void test() {
+
+		Optional<Product> productOptional = productRepository.findByTitleAndCategory_Name("iPhone", "Electronics");
+
+		System.out.println(productOptional.get().getTitle());
+	}
+
+	@Test
+	public void test2() {
+
+		List<Product> productOptional = productRepository.getByCategoryName("Electronics");
+
+		System.out.println(productOptional.get(0).getTitle()); // this get is underlined beecause that we need to handle the case when the list is empty but for simplicity we are not handling that case here
+	}
+
+	@Test
+	public void test3() {
+
+		Product productDetail = productRepository.getProductDetail2(1);
+
+		System.out.print(productDetail.getTitle());
+
+	}
+
+	@Test
+	public void test4() {
+
+		ProductTitleAndDescription productDetail = productRepository.getProductDetail3(1);
+
+		System.out.print(productDetail.getTitle() +  "," + productDetail.getDescription());
+
+	}
 }
+
